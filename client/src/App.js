74517d2bd9c1cb51da2093e1a106375eee980918
GlebZyryanov <darrenshen02@gmail.com>
2024-09-08 21:59:34 +0700

    init

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./components/Header";
+import styles from "./App.module.css";
+function App() {
+  return (
+    <div className={styles.Background}>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
